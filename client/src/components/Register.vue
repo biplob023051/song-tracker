1992ccf5e5e209af93a15d936f872cf33f7f5867
Biplob Sarkar <bcsarkar02@gmail.com>
2018-05-28 16:45:07 +0600

    Basic register post request from client to server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <input type="password" name="password" placeholder="password" v-model="password" />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,18 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express()
-
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('combined'))
-
-app.get('/status', (req, res) => {
-	res.send({
-		'Message': 'Hello World'
-	})
+app.post('/register', (req, res) => {
+  res.send({
+    'message': `Hello ${req.body.email}!`
+  })
 })
-
 const port = process.env.PORT || 8081
 app.listen(port, () => console.log(`App is listening on ${port}`))
